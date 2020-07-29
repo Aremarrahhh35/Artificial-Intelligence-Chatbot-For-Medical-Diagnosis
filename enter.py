4faaf7e210d698c5142e4c3a026b89bcc0c2a461
Aremarrahhh35 <42797460+Aremarrahhh35@users.noreply.github.com>
2020-07-29 10:54:45 +0100

    Add files via upload

---
+++ enter.py
@@ -0,0 +1,4 @@
+print("Enter the symptom you are experiencing  "
+      "\n Replace space with underscore e.g muscle_pain "
+      "\n When asked follow up questions fill yes/no "
+      "\n\t\t\t\t\t\t",end="->")
